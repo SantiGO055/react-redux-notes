5d73e89799cb271693356515882f3da61cc09ac4
SantiGO055 <santigonzalez05@gmail.com>
2022-05-06 17:22:20 -0300

    modify to two reducers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleImportanceOf } from './reducers/noteReducer'
+import NewNote from './components/NewNote';
+import Notes from './components/Notes';
+import VisibilityFilter from './components/VisibilityFilter';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div>
+        <NewNote />
+        <VisibilityFilter/>
+        <Notes/>
+      </div>
+    )
+  }
+  export default App;
+// const counterReducer = (state = 0, action) => {
+//   console.log(state)
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     case 'ZERO':
+//       return 0
+//     default:
+//       return state
+//   }
+// }
+// const store = createStore(counterReducer)
+// const App =()=> {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div>
+//           <h3>
+//             {store.getState()}
+//           </h3>
 
-export default App;
+//           <button onClick={e=>{store.dispatch({type:'INCREMENT'})}}>
+//             Increment
+//           </button>
+          
+//         </div>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { noteReducer, createNote } from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
+import {filterChange} from './reducers/filterReducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+export const store = createStore(reducer)
+
+store.subscribe(()=>console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer form many simple reducers'))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
+const renderApp = () => {
+  // ReactDOM.render(<App />, document.getElementById('root'))
+  //root.render(<App />);
+}
+
+// renderApp()
+// store.subscribe(renderApp) //cualquier cambio que se produzca vuelve a renderizar la app
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
